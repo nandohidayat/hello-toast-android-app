ae8388cd03459541403dbbe0d0b48b57b97f035f
Nando <mnandohidayat@gmail.com>
2018-09-22 10:10:37 +0700

    add Toast context view

--- app/src/main/java/com/nandohidayat/app/hellotoast/MainActivity.java
+++ app/src/main/java/com/nandohidayat/app/hellotoast/MainActivity.java
@@ -1,7 +1,10 @@
 package com.nandohidayat.app.hellotoast;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void showToast(View view) {
+        Context context = getApplicationContext();
+        Toast toast = Toast.makeText(context, R.string.toast_message, Toast.LENGTH_LONG);
+        toast.show();
+    }
+
+    public void countUp(View view) {
     }
 }
